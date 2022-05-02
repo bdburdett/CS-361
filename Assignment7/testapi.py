28902d10b53c4a44bd30f54ed57587709cd00577
bdburdett <burdetbe@oregonstate.edu>
2022-05-01 21:08:26 -0400

    time object created for teammate to use

--- Assignment7/testapi.py
+++ Assignment7/testapi.py
@@ -1,10 +1,16 @@
 import requests
 import json
 
+class Time:
+    def get_time(self):
+        response_API = requests.get('http://15ba-2603-9001-5504-e427-ed89-6eda-e851-bb6.ngrok.io')
 
-response_API = requests.get('http://15ba-2603-9001-5504-e427-ed89-6eda-e851-bb6.ngrok.io')
+        data = response_API.text
+        parse_json = json.loads(data)
+        active_case = parse_json['Timestamp']
+        print("The date is:", active_case)
 
-data = response_API.text
-parse_json = json.loads(data)
-active_case = parse_json['Timestamp']
-print("The date is:", active_case)
+
+time = Time()
+
+time.get_time()
